8fc9370531fb902a4b4882da67bfd50ab61664c0
DakeloX <dakelos2003@gmail.com>
2024-03-18 21:47:57 -0500

    Migración a nueva tecnología de desarrollo (Next.js)

---
+++ src/app/register/page.js
@@ -0,0 +1,11 @@
+import styles from "./../styles/register.module.css";
+
+export default function Register() {
+  return (
+    <main className={styles.fondo}>
+      <div>
+        Pagina de Registro
+      </div>
+    </main>
+  );
+}
