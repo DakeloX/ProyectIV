29703e42e3f5ab149cd431a5f1abb969f09d21a1
DakeloX <dakelos2003@gmail.com>
2024-03-21 00:17:53 -0500

    Ajuste de vista register y adición de combo box cargado con los roles que se encuentran en la base de datos

--- src/app/register/layout.js
+++ src/app/register/layout.js
@@ -7,7 +7,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={styles.fondo}>
+    <html lang="en">
       <body>{children}</body>
     </html>
   )
