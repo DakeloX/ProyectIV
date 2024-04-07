503e094d87d47f01bac529185f559370092cabc5
DakeloX <dakelos2003@gmail.com>
2024-04-07 13:35:07 -0500

    Enviroment configuration

--- src/app/api/addData/route.js
+++ src/app/api/addData/route.js
@@ -1,18 +1,18 @@
 import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
- 
+
 export async function GET(request) {
   const { searchParams } = new URL(request.url);
   const petName = searchParams.get('petName');
   const ownerName = searchParams.get('ownerName');
- 
+
   try {
     if (!petName || !ownerName) throw new Error('Pet and owner names required');
-    await sql`INSERT INTO Pets (Name, Owner) VALUES (${petName}, ${ownerName});`;
+    await sql`INSERT INTO Dogs (Name, Owner) VALUES (${petName}, ${ownerName});`;
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
- 
-  const pets = await sql`SELECT * FROM Pets;`;
+
+  const pets = await sql`SELECT * FROM Dogs;`;
   return NextResponse.json({ pets }, { status: 200 });
 }
--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,6 +1,5 @@
 'use client'
 import { useEffect, useState, useRef } from 'react';
-import { getRoles } from '../db';
 import styles from "./../styles/register.module.css";
 import Slider from "react-slick";
 import Image from 'next/image';
