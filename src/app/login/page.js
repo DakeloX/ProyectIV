666dd8db20f9e659dfe03eab1435fbf774991877
DakeloX <dakelos2003@gmail.com>
2024-04-24 22:04:20 -0500

    Manejo de erroes 1.2

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -37,7 +37,7 @@ export default function Login() {
             const res = await signIn("credentials", {
                 email: formData.email,
                 password: formData.password,
-                //userType: formData.typeAuthTable,
+                userType: formData.typeAuthTable,
                 redirect: false,
             });
 
