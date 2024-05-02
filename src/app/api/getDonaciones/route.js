4f3dbf5b8a76fbd9ed032942e1d04fa3c3448766
DakeloX <dakelos2003@gmail.com>
2024-05-02 15:54:28 -0500

    Ajustes dinamismo donaciones

--- src/app/api/getDonaciones/route.js
+++ src/app/api/getDonaciones/route.js
@@ -1,13 +1,17 @@
 import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
 
+
+const id = '1111111111';
+
 export async function GET() {
-    
-    const id = '1111111111'
     try {
-        const result = await sql`
-            SELECT * FROM donacion WHERE fundacion_id_fundacion = ${id}
+
+        const query = `
+            SELECT * FROM donacion WHERE fundacion_id_fundacion = $1
         `;
+        const result = await sql.query(query, [id]);
+
         return NextResponse.json(result.rows, { status: 200 });
     } catch (error) {
         console.error('Error al obtener donaciones:', error);
