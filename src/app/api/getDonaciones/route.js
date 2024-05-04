b80faf594649baaa0837d11d651646a7d378bd58
DakeloX <dakelos2003@gmail.com>
2024-05-03 19:17:46 -0500

    Actualizacion manejo cache

--- src/app/api/getDonaciones/route.js
+++ src/app/api/getDonaciones/route.js
@@ -1,18 +1,18 @@
 import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
 
-
-const id = '1111111111';
-
 export async function GET() {
     try {
-
+        const id = '1111111111';
         const query = `
             SELECT * FROM donacion WHERE fundacion_id_fundacion = $1
         `;
         const result = await sql.query(query, [id]);
 
-        return NextResponse.json(result.rows, { status: 200 });
+        // Deshabilitar el cache para asegurar la actualización en tiempo real
+        const response = NextResponse.json(result.rows, { status: 200 });
+        response.headers.set('Cache-Control', 'no-cache, no-store, must-revalidate');
+        return response;
     } catch (error) {
         console.error('Error al obtener donaciones:', error);
         return NextResponse.json({ error: 'Error al obtener donaciones' }, { status: 500 });
